62dc8ae041992d2bbc7188dfe04e94ed7cf490b2
Maria Garcia <mgarcia18@hawk.iit.edu>
2022-07-28 11:50:30 -0500

    removed 0

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var request = new XMLHttpRequest();
    request.open("GET", "data/orgs_and_zips.json", false);
    request.send(null)
    var my_JSON_object = JSON.parse(request.responseText);
-   alert (my_JSON_object.result[0]);
+   console.log(my_JSON_object.result);
 
 // function readSingleFile(e) {
 //     var file = e.target.files[0];

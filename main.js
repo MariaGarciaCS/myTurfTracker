8efe0aff6aec122af0fb9897ddeb8d57efc853cf
Maria Garcia <mgarcia18@hawk.iit.edu>
2022-07-28 11:52:06 -0500

    cleaned up

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// var request = new XMLHttpRequest();
-// request.open("GET", 'data/orgs_and_zips.json')
-// request.send(null)
 
-// var Orgs = JSON.parse(request.responseText);
 
 
 var request = new XMLHttpRequest();
@@ -11,21 +7,3 @@ var request = new XMLHttpRequest();
    var my_JSON_object = JSON.parse(request.responseText);
    console.log(my_JSON_object.result);
 
-// function readSingleFile(e) {
-//     var file = e.target.files[0];
-//     if (!file) {
-//       return;
-//     }
-//     var reader = new FileReader();
-//     reader.onload = function(e) {
-//       var contents = e.target.result;
-//       displayContents(contents);
-//     };
-//     reader.readAsText(file);
-//   }
-
-
-// const Orgs = JSON.parse(readSingleFile('data/zip_to_precinct.json'))
-// const jsonPrecints = 'data/zip_to_precinct.json'
-
-// console.log(Orgs)

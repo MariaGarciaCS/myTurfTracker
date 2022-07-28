564ac954784eafa851182d4cc6b3dc2cec57866a
Maria Garcia <mgarcia18@hawk.iit.edu>
2022-07-28 12:07:03 -0500

    new primary key system

--- file_manager.py
+++ file_manager.py
@@ -2,6 +2,22 @@
 import csv
 import json
  
+def make_json_orgs(csvFilePath, jsonFilePath):
+     
+    data = {}
+
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+
+        for rows in csvReader:
+            org = rows['Organization']
+            camp = rows['Campaign']
+            key = org + "-"+ camp
+            data[key] = rows
+ 
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
 def make_json(csvFilePath, jsonFilePath):
      
     data = {}
@@ -22,6 +38,6 @@ jsonOrgs= r'data/orgs_and_zips.json'
 csvPrecints = r'data/zip_to_precincts.csv'
 jsonPrecints= r'data/zip_to_precincts.json'
 
-make_json(csvOrgs, jsonOrgs)
+make_json_orgs(csvOrgs, jsonOrgs)
 make_json(csvPrecints, jsonPrecints)
 
